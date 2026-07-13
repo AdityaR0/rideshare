823abc7513b24c0ddedc50652f8e6f235d18bd0b
Aditya Raj <157242162+AdityaR0@users.noreply.github.com>
2026-07-13 23:54:55 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,13 +2,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://rideshare-api-xh6a.onrender.com/api",
 });
 
 // ✅ Automatically attach token to every request
 api.interceptors.request.use(
   (config) => {
-    // const token = localStorage.getItem("token");
     const token = localStorage.getItem("carpool-token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
