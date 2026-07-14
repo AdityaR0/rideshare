2aca3c2d7644747a1b41d4a1635beb0f91acd2e9
Aditya Raj <157242162+AdityaR0@users.noreply.github.com>
2026-07-14 17:36:10 +0530

    Update sendEmail.js

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -1,33 +1,28 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false, // true only for port 465
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-
-  connectionTimeout: 60000,
-  greetingTimeout: 60000,
-  socketTimeout: 60000,
 });
 
 const sendOTPEmail = async (email, otp) => {
-  // Check SMTP connection
-  await transporter.verify();
-  console.log("✅ SMTP Connected Successfully");
-
   await transporter.sendMail({
     from: `"RideShare" <${process.env.EMAIL_USER}>`,
     to: email,
     subject: "RideShare Community Verification OTP",
     html: `
       <h2>RideShare Community Verification</h2>
+
       <p>Your OTP is:</p>
+
       <h1>${otp}</h1>
+
       <p>This OTP will expire in 10 minutes.</p>
     `,
   });
